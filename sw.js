d763be2306ee893b7b1f7f558ec1762c39df6ff1
brilianeriko-95 <brilianeriko95@gmail.com>
2026-03-08 08:23:06 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'plant-monitor-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/config.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
